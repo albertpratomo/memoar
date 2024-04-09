6eec2be9035c0da5ab6d5f8092295cea9b57d836
Albert Pratomo <albertpratomo21@gmail.com>
2024-04-10 00:09:12 +0200

    feat: create simple index page

---
+++ resources/js/Pages/Index.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { Head } from '@inertiajs/vue3';
+</script>
+
+<template>
+    <Head title="Memoar" />
+
+    <div class="h-screen flex items-center justify-center">
+        <h1>Memoar</h1>
+    </div>
+</template>
